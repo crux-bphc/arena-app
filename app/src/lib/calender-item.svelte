3c49a6a5fa2a92074e51d31c85fb8f2ecbef49b9
Sai Vardhan <vardhan.6405@gmail.com>
2024-12-28 12:41:32 +0530

    feat: added event card grid and event card component

---
+++ app/src/lib/calender-item.svelte
@@ -0,0 +1,9 @@
+<script>
+	let { colStart, rowStart, rowSpan } = $props();
+</script>
+
+<div
+	class="col-span-1 bg-blue-500"
+	style="grid-column-start: {colStart};
+		grid-row: {rowStart} / span {rowSpan};"
+></div>
--- app/src/lib/calender.svelte
+++ app/src/lib/calender.svelte
@@ -1,4 +1,6 @@
 <script>
+	import CalenderItem from './calender-item.svelte';
+
 	// calender starts at {startTime} AM
 	let { startTime = 6 } = $props();
 
@@ -18,16 +20,31 @@
 		<div class="text-center">12 AM</div>
 	</div>
 
-	<!-- background grid -->
-	<div class="grid-cols-{cols} grid-rows-{rows} grid w-[calc(100%-3rem)] py-3">
-		{#each { length: rows * cols }, i}
-			<div
-				class={'h-20 border-t-[1px] border-white/50' +
-					' ' +
-					(rows * cols - i <= cols && 'border-b-[1px]') +
-					' ' +
-					(i % cols !== 0 && 'border-l-[1px]')}
-			></div>
-		{/each}
+	<div class="relative my-3 w-[calc(100%-3rem)]">
+		<!-- background grid -->
+		<div
+			class="grid"
+			style="grid-template-columns: repeat({cols}, minmax(0, 1fr)); 
+                grid-template-rows: repeat({rows}, minmax(0, 1fr))"
+		>
+			{#each { length: rows * cols }, i}
+				<div
+					class="h-20 border-t-[1px] border-white/50
+                        {rows * cols - i <= cols && 'border-b-[1px]'}
+                        {i % cols !== 0 && 'border-l-[1px]'}"
+				></div>
+			{/each}
+		</div>
+
+		<!-- event card grid -->
+		<div
+			class="absolute inset-0 grid h-full"
+			style="grid-template-columns: repeat({cols}, minmax(0, 1fr)); 
+                grid-template-rows: repeat({rows * 4}, minmax(0, 1fr))"
+		>
+			<CalenderItem colStart="1" rowStart="14" rowSpan="13" />
+			<CalenderItem colStart="2" rowStart="24" rowSpan="5" />
+			<CalenderItem colStart="3" rowStart="5" rowSpan="10" />
+		</div>
 	</div>
 </div>
