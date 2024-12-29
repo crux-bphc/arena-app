8faaa60b10716f01066827f2dc4c18146e4f3faf
skoriop <karthikp.ivy@gmail.com>
2024-12-29 20:31:54 +0530

    feat: add auth guard redirects

--- app/src/hooks.server.ts
+++ app/src/hooks.server.ts
@@ -1,9 +1,10 @@
 import PocketBase from 'pocketbase';
-import { type Handle } from '@sveltejs/kit';
+import { redirect, type Handle } from '@sveltejs/kit';
 import { PB_URL } from '$env/static/private';
 import type { TypedPocketBase } from '$lib/types/pocketbase';
+import { sequence } from '@sveltejs/kit/hooks';
 
-export const handle: Handle = async ({ event, resolve }) => {
+const authentication: Handle = async ({ event, resolve }) => {
 	event.locals.pb = new PocketBase(PB_URL) as TypedPocketBase;
 	event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
 
@@ -19,13 +20,26 @@ export const handle: Handle = async ({ event, resolve }) => {
 
 	const response = await resolve(event);
 
-	// apparently browsers don't like mixing redirects with cookies with SameSite=Strict
+	// so browsers don't like mixing oauth (external) redirects with cookies that have SameSite=Strict
 	// https://github.com/pocketbase/pocketbase/discussions/903#discussioncomment-4369424
-	// is this a security risk? probably not, SameSite=Lax sends cookies mainly for top-level redirects (like users clicking on links).
-	// since the only external link in our application is the google oauth2 redirect, it should be fine, but it is still worth noting.
+	// is this a security risk? probably not, SameSite=Lax sends cookies mainly for top-level redirects (like users clicking on links)
+	// since the only external link in our application is the google oauth2 redirect, it should be fine, but it is still worth noting
 	response.headers.append(
 		'set-cookie',
-		event.locals.pb.authStore.exportToCookie({ sameSite: 'Lax' })
+		event.locals.pb.authStore.exportToCookie({ sameSite: 'lax' })
 	);
 	return response;
 };
+
+const authorization: Handle = async ({ event, resolve }) => {
+	if (!event.route.id?.startsWith('/(auth)/')) {
+		if (!event.locals.pb.authStore.record) {
+			return redirect(303, '/login');
+		}
+	}
+
+	const response = await resolve(event);
+	return response;
+};
+
+export const handle = sequence(authentication, authorization);
