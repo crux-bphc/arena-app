26551680bdbe6c28ae0642446b5a763a36a01a8a
skoriop <karthikp.ivy@gmail.com>
2024-12-30 23:53:05 +0530

    refactor: remove unused tailwind classes

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -5,14 +5,6 @@ export default {
 
 	theme: {
 		extend: {
-			gridTemplateRows: {
-				'13': 'repeat(13, minmax(0, 1fr))',
-				'14': 'repeat(14, minmax(0, 1fr))',
-				'15': 'repeat(15, minmax(0, 1fr))',
-				'16': 'repeat(16, minmax(0, 1fr))',
-				'17': 'repeat(17, minmax(0, 1fr))',
-				'18': 'repeat(18, minmax(0, 1fr))'
-			},
 			colors: {
 				primary: '#2C2378'
 			}
