5dd7791682595f0d33a02ff074215872bc920b4a
Sai Vardhan <vardhan.6405@gmail.com>
2024-12-31 17:31:21 +0530

    feat: added navigation to navbar

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -9,20 +9,27 @@
 </script>
 
 {@render children()}
-<div class="fixed inset-x-0 bottom-0 w-full xs:p-4 p-3">
+<div class="xs:p-4 fixed inset-x-0 bottom-0 w-full p-3">
 	<div
-		class="bg-accent/10 flex w-full flex-row items-center justify-around xs:rounded-3xl rounded-2xl xs:p-4 p-2 backdrop-blur-lg"
+		class="bg-accent/10 xs:rounded-3xl xs:p-4 flex w-full flex-row items-center justify-around rounded-2xl p-2 backdrop-blur-lg"
 	>
-		<Button variant="ghost" class="rounded-full ">
+		<!-- Home / Events page buttom -->
+		<Button variant="ghost" href="/" class="rounded-full ">
 			<IconHomeFilled class="text-text xs:size-8 size-6" />
 		</Button>
-		<Button variant="ghost" class="rounded-full ">
+
+		<!-- loaderboard page button -->
+		<Button variant="ghost" href="/leaderboard" class="rounded-full ">
 			<IconFlagFilled class="text-text xs:size-8 size-6" />
 		</Button>
-		<Button variant="ghost" class="rounded-full ">
+
+		<!-- Calender page button -->
+		<Button variant="ghost" href="/calender" class="rounded-full ">
 			<IconCalendarEvent class="text-text xs:size-8 size-6" />
 		</Button>
-		<Button variant="ghost" class="rounded-full ">
+
+		<!-- Profile page button -->
+		<Button variant="ghost" href="/profile" class="rounded-full ">
 			<!-- add avatar to this -->
 			<div class="bg-foreground xs:size-8 size-6 rounded-full"></div>
 		</Button>
