9e8eee8ca456487f799a3fb28b2a9fe39cb76e40
Sai Vardhan <vardhan.6405@gmail.com>
2024-12-31 17:21:45 +0530

    fix: made navbar responsive for even smaller screens

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -9,22 +9,22 @@
 </script>
 
 {@render children()}
-<div class="fixed inset-x-0 bottom-0 w-full p-4">
+<div class="fixed inset-x-0 bottom-0 w-full xs:p-4 p-3">
 	<div
-		class="bg-accent/10 flex w-full flex-row items-center justify-around rounded-3xl p-4 backdrop-blur-lg"
+		class="bg-accent/10 flex w-full flex-row items-center justify-around xs:rounded-3xl rounded-2xl xs:p-4 p-2 backdrop-blur-lg"
 	>
-		<Button variant="ghost" class=" rounded-full ">
-			<IconHomeFilled class="text-text size-8" />
+		<Button variant="ghost" class="rounded-full ">
+			<IconHomeFilled class="text-text xs:size-8 size-6" />
 		</Button>
-		<Button variant="ghost" class=" rounded-full ">
-			<IconFlagFilled class="text-text size-8" />
+		<Button variant="ghost" class="rounded-full ">
+			<IconFlagFilled class="text-text xs:size-8 size-6" />
 		</Button>
-		<Button variant="ghost" class=" rounded-full ">
-			<IconCalendarEvent class="text-text size-8" />
+		<Button variant="ghost" class="rounded-full ">
+			<IconCalendarEvent class="text-text xs:size-8 size-6" />
 		</Button>
-		<Button variant="ghost" class=" rounded-full ">
+		<Button variant="ghost" class="rounded-full ">
 			<!-- add avatar to this -->
-			<div class="bg-foreground size-8 rounded-full"></div>
+			<div class="bg-foreground xs:size-8 size-6 rounded-full"></div>
 		</Button>
 	</div>
 </div>
