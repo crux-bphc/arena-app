fffe55ef4dd3461d9b3104daecdb2b6bd452575c
Sai Vardhan <vardhan.6405@gmail.com>
2024-12-31 17:12:28 +0530

    feat: added basic navbar buttons

--- app/src/lib/components/ui/button/index.ts
+++ app/src/lib/components/ui/button/index.ts
@@ -3,7 +3,7 @@ import type { Button as ButtonPrimitive } from "bits-ui";
 import Root from "./button.svelte";
 
 const buttonVariants = tv({
-	base: "ring-offset-background focus-visible:ring-ring inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+	base: "ring-offset-background focus-visible:ring-ring inline-flex items-center justify-center whitespace-nowrap rounded-full text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
 	variants: {
 		variant: {
 			default: "bg-primary text-primary-foreground hover:bg-primary/90",
@@ -11,7 +11,7 @@ const buttonVariants = tv({
 			outline:
 				"border-input bg-background hover:bg-accent hover:text-accent-foreground border",
 			secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-			ghost: "hover:bg-accent hover:text-accent-foreground",
+			ghost: "hover:bg-accent/30 hover:text-accent-foreground",
 			link: "text-primary underline-offset-4 hover:underline",
 		},
 		size: {
--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,6 +1,30 @@
 <script lang="ts">
 	import '../app.css';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { IconHomeFilled } from '@tabler/icons-svelte';
+	import { IconFlagFilled } from '@tabler/icons-svelte';
+	import { IconCalendarEvent } from '@tabler/icons-svelte';
+
 	let { children } = $props();
 </script>
 
 {@render children()}
+<div class="fixed inset-x-0 bottom-0 w-full p-4">
+	<div
+		class="bg-accent/10 flex w-full flex-row items-center justify-around rounded-3xl p-4 backdrop-blur-lg"
+	>
+		<Button variant="ghost" class=" rounded-full ">
+			<IconHomeFilled class="text-text size-8" />
+		</Button>
+		<Button variant="ghost" class=" rounded-full ">
+			<IconFlagFilled class="text-text size-8" />
+		</Button>
+		<Button variant="ghost" class=" rounded-full ">
+			<IconCalendarEvent class="text-text size-8" />
+		</Button>
+		<Button variant="ghost" class=" rounded-full ">
+			<!-- add avatar to this -->
+			<div class="bg-foreground size-8 rounded-full"></div>
+		</Button>
+	</div>
+</div>
