7c15dbc36f4014e5299ba3571420793442a23e05
skoriop <karthikp.ivy@gmail.com>
2025-01-03 14:59:50 +0530

    fix: fix internal server error when no filter applied

--- app/src/routes/api/events/+server.ts
+++ app/src/routes/api/events/+server.ts
@@ -7,13 +7,13 @@ const handleGET: RequestHandler = async ({ url }: { url: URL }) => {
 	// Filter by sport
 	const sport = url.searchParams.get('sport');
 	try {
-        const filter = sport ? `sport="${sport}"` : undefined;
-        const events: EventsRecord[] = await pb.collection('events').getFullList({ filter });
+		const options = sport ? { filter: `sport="${sport}"` } : {};
+		const events: EventsRecord[] = await pb.collection('events').getFullList(options);
 
 		return json({ events });
 	} catch (err) {
 		return error(500, `Failed to get events due to error: ${err}`);
 	}
-}
+};
 
 export const GET = handleGET;
