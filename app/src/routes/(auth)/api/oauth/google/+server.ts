1f667fea57a6ab099e048acbb04825f887c3a94b
unknown <user.soem1@gmail.com>
2025-01-08 13:42:02 +0530

    refactor: Use pocketbase to give new user a starting balance

--- app/src/routes/(auth)/api/oauth/google/+server.ts
+++ app/src/routes/(auth)/api/oauth/google/+server.ts
@@ -1,9 +1,8 @@
 import { error, redirect } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 import { PUBLIC_AUTH_REDIRECT_URL } from '$env/static/public';
-import { STARTING_BALANCE } from '$env/static/private';
 import type { AuthProviderInfo } from 'pocketbase';
-import pb from '$lib/server/database';
+import { STARTING_BALANCE } from '$env/static/private';
 
 export const GET: RequestHandler = async ({ url, cookies, locals }) => {
 	const code = url.searchParams.get('code');
@@ -18,19 +17,10 @@ export const GET: RequestHandler = async ({ url, cookies, locals }) => {
 		return error(403, 'Invalid auth state');
 	}
 
-	// Get the time before the user is authenticated to determine the state of the user (new / old)
-	const now = Date.now();
-
 	const { record: user } = await locals.pb
 		.collection('users')
-		.authWithOAuth2Code(provider.name, code, provider.codeVerifier, PUBLIC_AUTH_REDIRECT_URL);
+		.authWithOAuth2Code(provider.name, code, provider.codeVerifier, PUBLIC_AUTH_REDIRECT_URL, { balance: STARTING_BALANCE });
 	console.log(user);
-
-	// This is a new user, since the created time is greater than the time when the authentication was queried
-	if (new Date(user.created).getTime() > now) {
-		const newUser = await pb.collection('users').update(user.id, { balance: STARTING_BALANCE});
-		console.log('Updated new user balance:', newUser.balance);
-	}
 
 	return redirect(302, '/');
 };
