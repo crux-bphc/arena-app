c49f9b0c9639df4effd52fc191cab56855556509
unknown <user.soem1@gmail.com>
2025-01-08 11:25:52 +0530

    fix: Give starting balance to new user

--- app/src/routes/(auth)/api/oauth/google/+server.ts
+++ app/src/routes/(auth)/api/oauth/google/+server.ts
@@ -1,7 +1,9 @@
 import { error, redirect } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 import { PUBLIC_AUTH_REDIRECT_URL } from '$env/static/public';
+import { STARTING_BALANCE } from '$env/static/private';
 import type { AuthProviderInfo } from 'pocketbase';
+import pb from '$lib/server/database';
 
 export const GET: RequestHandler = async ({ url, cookies, locals }) => {
 	const code = url.searchParams.get('code');
@@ -16,10 +18,19 @@ export const GET: RequestHandler = async ({ url, cookies, locals }) => {
 		return error(403, 'Invalid auth state');
 	}
 
+	// Get the time before the user is authenticated to determine the state of the user (new / old)
+	const now = Date.now();
+
 	const { record: user } = await locals.pb
 		.collection('users')
 		.authWithOAuth2Code(provider.name, code, provider.codeVerifier, PUBLIC_AUTH_REDIRECT_URL);
 	console.log(user);
+
+	// This is a new user, since the created time is greater than the time when the authentication was queried
+	if (new Date(user.created).getTime() > now) {
+		const newUser = await pb.collection('users').update(user.id, { balance: STARTING_BALANCE});
+		console.log('Updated new user balance:', newUser.balance);
+	}
 
 	return redirect(302, '/');
 };
