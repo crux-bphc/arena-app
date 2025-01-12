cbbb04ef1cdea3737d1a274776e6946b259b911f
skoriop <karthikp.ivy@gmail.com>
2025-01-13 05:05:15 +0530

    feat: add some checks for bet payout

--- db/hooks/bet_status_update.pb.js
+++ db/hooks/bet_status_update.pb.js
@@ -4,12 +4,25 @@ onRecordUpdate((e) => {
     return e.next();
   }
 
+  // unfortunately pocketbase admin UI doesn't support showing timestamps in local time and defaults to UTC
+  // i don't think it's realistic or convenient to expect them to convert local time to UTC while adding events
+  // so this means a lot of reverse timezone shenanigans will be needed in the frontend too
+  let now = new Date(new Date().getTime() + 1000 * 60 * 330);
+  
+  if (now / 1000 < e.record.getDateTime("endTime").unix()) {
+    throw new BadRequestError("Failed to trigger bet payout: Event has not ended yet!");
+  }
+
   const winners = $app
     .findAllRecords(
       "standings",
       $dbx.exp("position == 1 AND event == {:id}", { id: event.id })
     )
     .map((standing) => standing.get("team"));
+  
+  if (!winners.length) {
+    throw new BadRequestError("Failed to trigger bet payout: No winners found!");
+  }
 
   const bets = $app.findAllRecords(
     "bets",
