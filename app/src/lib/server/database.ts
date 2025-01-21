fac30acb6c7e6e1e0d548cdc566b59d6865c7660
Sai Vardhan <vardhan.6405@gmail.com>
2025-01-21 23:25:00 +0530

    fix: stopped connecting to db if code is building

--- app/src/lib/server/database.ts
+++ app/src/lib/server/database.ts
@@ -1,3 +1,4 @@
+import { building } from '$app/environment';
 import PocketBase from 'pocketbase';
 import { INTERNAL_PB_URL, PB_SUPERUSER_EMAIL, PB_SUPERUSER_PASSWORD } from '$env/static/private';
 import type { TypedPocketBase } from '$lib/types/pocketbase';
@@ -5,8 +6,10 @@ import type { TypedPocketBase } from '$lib/types/pocketbase';
 const pb = new PocketBase(INTERNAL_PB_URL) as TypedPocketBase;
 pb.autoCancellation(false);
 
-await pb
-	.collection('_superusers')
-	.authWithPassword(PB_SUPERUSER_EMAIL, PB_SUPERUSER_PASSWORD, { autoRefreshThreshold: 30 * 60 });
+if (!building) {
+	await pb
+		.collection('_superusers')
+		.authWithPassword(PB_SUPERUSER_EMAIL, PB_SUPERUSER_PASSWORD, { autoRefreshThreshold: 30 * 60 });
+}
 
 export default pb;
