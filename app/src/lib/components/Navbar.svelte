c86eb185b2ead65f40848bd8640089569f163765
Sai Vardhan <vardhan.6405@gmail.com>
2025-01-24 15:59:20 +0530

    fix: reordered navbar and minor style fixes

--- app/src/lib/components/Navbar.svelte
+++ app/src/lib/components/Navbar.svelte
@@ -12,33 +12,35 @@
 	let { user }: { user: UsersResponse | undefined } = $props();
 </script>
 
-<div class="xs:p-4 fixed inset-x-0 bottom-0 w-full p-3">
+<div
+	class="xs:px-4 to-background via-background/80 fixed inset-x-0 bottom-0 w-full bg-gradient-to-b from-transparent px-2 py-4"
+>
 	<div
-		class="bg-accent/35 xs:p-4 flex w-full flex-row items-center justify-around rounded-full p-2 backdrop-blur-lg"
+		class="bg-secondary flex w-full flex-row items-center justify-between rounded-full border-[1.5px] p-3"
 	>
 		<!-- Home / Events page buttom -->
 		<Button variant="ghost" size="icon" href="/" class="rounded-full ">
-			<IconHome stroke={1.5} class="xs:size-8 size-6" />
+			<IconHome stroke={1.5} class="size-8" />
 		</Button>
 
 		<!-- Sports page button -->
 		<Button variant="ghost" size="icon" href="/sports" class="rounded-full ">
-			<IconBallBasketball stroke={1} class="xs:size-8 size-6" />
-		</Button>
-
-		<!-- Leaderboard page button -->
-		<Button variant="ghost" size="icon" href="/leaderboard" class="rounded-full ">
-			<IconChartBar stroke={1.5} class="xs:size-8 size-6" />
+			<IconBallBasketball stroke={1.5} class="size-8" />
 		</Button>
 
 		<!-- Calender page button -->
 		<Button variant="ghost" size="icon" href="/calender" class="rounded-full ">
-			<IconCalendarWeek stroke={2} class="xs:size-8 size-6" />
+			<IconCalendarWeek stroke={1.5} class="size-8" />
+		</Button>
+
+		<!-- Leaderboard page button -->
+		<Button variant="ghost" size="icon" href="/leaderboard" class="rounded-full ">
+			<IconChartBar stroke={1.5} class="size-8" />
 		</Button>
 
 		<!-- Profile page button -->
 		<Button variant="ghost" size="icon" href="/profile" class="rounded-full ">
-			<div class="bg-foreground xs:size-8 size-6 rounded-full">
+			<div class="size-8 rounded-full">
 				{#if user}
 					<img
 						src={`${PUBLIC_PB_URL}/api/files/users/${user.id}/${user.avatar}`}
