157b9548f2c9a4e1d15b19f9a056ca978af31874
Sai Vardhan <vardhan.6405@gmail.com>
2025-01-24 15:26:39 +0530

    feat: updated navbar icons

--- app/src/lib/components/Navbar.svelte
+++ app/src/lib/components/Navbar.svelte
@@ -2,34 +2,42 @@
 	import { PUBLIC_PB_URL } from '$env/static/public';
 	import { Button } from '$lib/components/ui/button/index.js';
 	import type { UsersResponse } from '$lib/types/pocketbase';
-	import { IconHomeFilled } from '@tabler/icons-svelte';
-	import { IconFlagFilled } from '@tabler/icons-svelte';
-	import { IconCalendarEvent } from '@tabler/icons-svelte';
+	import {
+		IconBallBasketball,
+		IconCalendarWeek,
+		IconChartBar,
+		IconHome
+	} from '@tabler/icons-svelte';
 
 	let { user }: { user: UsersResponse | undefined } = $props();
 </script>
 
 <div class="xs:p-4 fixed inset-x-0 bottom-0 w-full p-3">
 	<div
-		class="bg-accent/35 xs:rounded-3xl xs:p-4 flex w-full flex-row items-center justify-around rounded-2xl p-2 backdrop-blur-lg"
+		class="bg-accent/35 xs:p-4 flex w-full flex-row items-center justify-around rounded-full p-2 backdrop-blur-lg"
 	>
 		<!-- Home / Events page buttom -->
-		<Button variant="ghost" href="/" class="rounded-full ">
-			<IconHomeFilled class="xs:size-8 size-6" />
+		<Button variant="ghost" size="icon" href="/" class="rounded-full ">
+			<IconHome stroke={1.5} class="xs:size-8 size-6" />
+		</Button>
+
+		<!-- Sports page button -->
+		<Button variant="ghost" size="icon" href="/sports" class="rounded-full ">
+			<IconBallBasketball stroke={1} class="xs:size-8 size-6" />
 		</Button>
 
 		<!-- Leaderboard page button -->
-		<Button variant="ghost" href="/leaderboard" class="rounded-full ">
-			<IconFlagFilled class="xs:size-8 size-6" />
+		<Button variant="ghost" size="icon" href="/leaderboard" class="rounded-full ">
+			<IconChartBar stroke={1.5} class="xs:size-8 size-6" />
 		</Button>
 
 		<!-- Calender page button -->
-		<Button variant="ghost" href="/calender" class="rounded-full ">
-			<IconCalendarEvent class="xs:size-8 size-6" />
+		<Button variant="ghost" size="icon" href="/calender" class="rounded-full ">
+			<IconCalendarWeek stroke={2} class="xs:size-8 size-6" />
 		</Button>
 
 		<!-- Profile page button -->
-		<Button variant="ghost" href="/profile" class="rounded-full ">
+		<Button variant="ghost" size="icon" href="/profile" class="rounded-full ">
 			<div class="bg-foreground xs:size-8 size-6 rounded-full">
 				{#if user}
 					<img
