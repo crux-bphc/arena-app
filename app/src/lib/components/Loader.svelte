f59101a25a90379639ce8f1a6699275b094b0842
Anirudh <anirudh.mss@gmail.com>
2025-01-24 15:33:53 +0530

    chore: convert raw css to tailwind

---
+++ app/src/lib/components/Loader.svelte
@@ -0,0 +1,4 @@
+<div class="flex flex-col items-center justify-center h-48">
+  <div class="w-10 h-10 border-4 border-t-white border-white/30 rounded-full animate-spin"></div>
+  <p class="mt-3 text-lg text-white">Loading...</p>
+</div>
