8653ab26fbf86da637a17d3fe997ca9c5e22379b
skoriop <karthikp.ivy@gmail.com>
2025-01-25 21:51:14 +0530

    style: make indentation consistent

--- sports_db/hooks/standings_update_validate.pb.js
+++ sports_db/hooks/standings_update_validate.pb.js
@@ -1,34 +1,36 @@
 onRecordUpdate((e) => {
-  const event = e.record;
+	const event = e.record;
 
-  if (!event.getBool("standingsUpdated")) {
-    if (event.original().getBool("standingsUpdated")) {
-      throw new BadRequestError("Cannot unset standingsUpdated! Bet payouts have already been triggered!");
-    }
-    return e.next();
-  }
+	if (!event.getBool("standingsUpdated")) {
+		if (event.original().getBool("standingsUpdated")) {
+			throw new BadRequestError(
+				"Cannot unset standingsUpdated! Bet payouts have already been triggered!"
+			);
+		}
+		return e.next();
+	}
 
-  // unfortunately pocketbase admin UI doesn't support showing timestamps in local time and defaults to UTC
-  // i don't think it's realistic or convenient to expect them to convert local time to UTC while adding events
-  // so this means a lot of reverse timezone shenanigans will be needed in the frontend too
-  let now = new Date(new Date().getTime() + 1000 * 60 * 330);
+	// unfortunately pocketbase admin UI doesn't support showing timestamps in local time and defaults to UTC
+	// i don't think it's realistic or convenient to expect them to convert local time to UTC while adding events
+	// so this means a lot of reverse timezone shenanigans will be needed in the frontend too
+	let now = new Date(new Date().getTime() + 1000 * 60 * 330);
 
-  if (now / 1000 < e.record.getDateTime("endTime").unix()) {
-    throw new BadRequestError(
-      "Failed to trigger bet payout: Event has not ended yet!"
-    );
-  }
+	if (now / 1000 < e.record.getDateTime("endTime").unix()) {
+		throw new BadRequestError(
+			"Failed to trigger bet payout: Event has not ended yet!"
+		);
+	}
 
-  const winners = $app.findAllRecords(
-    "standings",
-    $dbx.exp("position == 1 AND event == {:id}", { id: event.id })
-  );
+	const winners = $app.findAllRecords(
+		"standings",
+		$dbx.exp("position == 1 AND event == {:id}", { id: event.id })
+	);
 
-  if (!winners.length) {
-    throw new BadRequestError(
-      "Failed to trigger bet payout: No winners found!"
-    );
-  }
+	if (!winners.length) {
+		throw new BadRequestError(
+			"Failed to trigger bet payout: No winners found!"
+		);
+	}
 
-  e.next();
+	e.next();
 }, "events");
