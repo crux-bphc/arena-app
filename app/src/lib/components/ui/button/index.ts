b060a775baf828f2cba58787c05280177459f644
Radian6405 <vardhan.6405@gmail.com>
2025-01-28 22:25:07 +0530

    feat: added sport selection sidebar

--- app/src/lib/components/ui/button/index.ts
+++ app/src/lib/components/ui/button/index.ts
@@ -3,15 +3,15 @@ import type { Button as ButtonPrimitive } from 'bits-ui';
 import Root from './button.svelte';
 
 const buttonVariants = tv({
-	base: 'relative group ring-offset-background focus-visible:ring-ring inline-flex items-center justify-center whitespace-nowrap rounded-full text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+	base: 'relative group ring-offset-background focus-visible:ring-ring inline-flex items-center justify-center whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
 	variants: {
 		variant: {
 			default: 'bg-primary text-primary-foreground hover:bg-primary/90',
 			destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
-			outline: 'border-input bg-background hover:bg-accent hover:text-accent-foreground border',
+			outline: 'border-input bg-background hover:bg-secondary hover:text-secondary-foreground border',
 			secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
 			ghost: 'hover:bg-accent hover:text-accent-foreground',
-			navbar: 'group-[.on-page]:text-primary-foreground',
+			navbar: 'group-[.on-page]:text-primary-foreground rounded-full',
 			link: 'text-primary underline-offset-4 hover:underline'
 		},
 		size: {
--- app/src/routes/sports/+page.svelte
+++ app/src/routes/sports/+page.svelte
@@ -1,1 +1,52 @@
-<h1>sports</h1>
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+	import { ListFilter } from 'lucide-svelte';
+
+	let activeSport = $state('football');
+	let showFilter = $state(true);
+
+	let sports = [
+		'all sports',
+		'football',
+		'skating',
+		'squash',
+		'table tennis',
+		'pool',
+		'hockey',
+		'chess',
+		'tennis',
+		'cricket',
+		'athletics',
+		'powerlifting',
+		'swimming',
+		'carrom',
+		'throwball',
+		'kabbadi',
+		'badminton',
+		'snooker',
+		'volleyball',
+		'ultimate frisbee'
+	];
+</script>
+
+<Button variant="outline" size="icon" class="mx-4 my-2" onclick={() => (showFilter = !showFilter)}>
+	<ListFilter class="size-8" />
+</Button>
+<div class="flex h-screen flex-row gap-2 px-2">
+	<div
+		class="hide-scrollbar bg-secondary box-content w-1/3 flex-wrap gap-2 overflow-y-auto p-2 [direction:rtl] {showFilter
+			? 'flex'
+			: 'hidden'}"
+	>
+		{#each sports as sport}
+			<Button
+				class="text-md h-14 w-full rounded-xl capitalize"
+				variant={activeSport === sport ? 'default' : 'secondary'}
+				onclick={() => (activeSport = sport)}
+			>
+				{sport}
+			</Button>
+		{/each}
+	</div>
+
+</div>
