ecee3b07b4b24ace1ac577d294c7605e80b81403
Radian6405 <vardhan.6405@gmail.com>
2025-01-29 23:04:10 +0530

    feat: added sidebar toggle

--- app/src/routes/sports/+page.svelte
+++ app/src/routes/sports/+page.svelte
@@ -1,50 +1,63 @@
 <script lang="ts">
+	import EventCard from '$lib/components/EventCard.svelte';
 	import Button from '$lib/components/ui/button/button.svelte';
 	import { EventsSportOptions } from '$lib/types/enums';
 	import { type EventsRecord } from '$lib/types/pocketbase';
 
 	import { ListFilter } from 'lucide-svelte';
 
-	let activeSport = $state('football');
-	let showFilter = $state(true);
+	let activeSport: string = $state(EventsSportOptions.football);
+	let showSidebar = $state(true);
+	let showAllSports = $state(false);
 
 	let sports: EventsSportOptions[] = Object.values(EventsSportOptions);
-	let events: EventsRecord[] = [
-		{
-			banner: '',
-			created: '2025-01-28 17:16:45.944Z',
-			description: 'test',
-			endTime: '2025-01-31 12:00:00.000Z',
-			id: 'so9o0ig2dn44r81',
-			location: 'here',
-			sport: EventsSportOptions['football'],
-			standingsUpdated: false,
-			startTime: '2025-01-01 12:00:00.000Z',
-			teams: ['8j2p6rabfu3z9uj', 'qxwt6525j8p6616'],
-			title: 'event1',
-			updated: '2025-01-28 17:16:45.944Z'
-		}
-	];
+	let events: EventsRecord[] = [];
 </script>
 
-<Button variant="outline" size="icon" class="mx-4 my-2" onclick={() => (showFilter = !showFilter)}>
+<!-- filter button -->
+<Button
+	variant="secondary"
+	size="icon"
+	class="mx-4 my-2"
+	onclick={() => (showSidebar = !showSidebar)}
+>
 	<ListFilter class="size-8" />
 </Button>
-<div class="flex h-screen flex-row gap-2 px-2">
+
+<div class="flex h-screen flex-row gap-2 {showSidebar ? 'pr-3' : 'px-3'}">
+	<!-- side bar -->
 	<div
-		class="hide-scrollbar bg-secondary box-content w-1/3 flex-wrap gap-2 overflow-y-auto p-2 [direction:rtl] {showFilter
+		class="hide-scrollbar bg-secondary box-content w-1/3 flex-wrap gap-2 overflow-y-auto p-2 [direction:rtl] {showSidebar
 			? 'flex'
 			: 'hidden'}"
 	>
+		<Button
+			class="text-md h-14 w-full rounded-xl capitalize"
+			variant={showAllSports ? 'default' : 'secondary'}
+			onclick={() => (showAllSports = true)}
+		>
+			All sports
+		</Button>
 		{#each sports as sport}
 			<Button
 				class="text-md h-14 w-full rounded-xl capitalize"
-				variant={activeSport === sport ? 'default' : 'secondary'}
-				onclick={() => (activeSport = sport)}
+				variant={!showAllSports && activeSport === sport ? 'default' : 'secondary'}
+				onclick={() => {
+					activeSport = sport;
+					showAllSports = false;
+				}}
 			>
 				{sport}
 			</Button>
 		{/each}
 	</div>
 
+	<!-- event cards -->
+	<div
+		class="hide-scrollbar flex flex-wrap gap-2 overflow-y-auto {showSidebar ? 'w-2/3' : 'w-full'}"
+	>
+		{#each events as event}
+			<EventCard isMinimized={showSidebar} {event} />
+		{/each}
+	</div>
 </div>
