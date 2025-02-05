b3de2e875ed70c24faff8c45c8eeb9e5432e550d
skoriop <karthikp.ivy@gmail.com>
2025-02-05 19:35:47 +0530

    style: lint with prettier

--- app/src/routes/(auth)/login/+page.svelte
+++ app/src/routes/(auth)/login/+page.svelte
@@ -1,24 +1,29 @@
-<style>
-    .background {
-        background: url('$lib/assets/images/login/spots.png')
-    }
-</style>
-
 <script lang="ts">
-    import Button from "$lib/components/ui/button/button.svelte";
-    import header from "$lib/assets/images/login/header.svg";
-    import google from "$lib/assets/images/login/google.svg";
+	import Button from '$lib/components/ui/button/button.svelte';
+	import header from '$lib/assets/images/login/header.svg';
+	import google from '$lib/assets/images/login/google.svg';
 </script>
-<div class="background flex flex-col gap-7 justify-center items-center font-bold h-full absolute w-full select-none">
-    <!-- Arena 2025 svg logo because css is hard -->
-    <img src={header} alt="Arena 2025 header" class="pointer-events-none" />
-    <a href="/api/oauth">
-        <Button class="bg-white hover:bg-gray-300 active:bg-grey-300 font-semibold w-64 rounded-xl flex gap-2">
-            <img src={google} alt="Arena 2025 header" class="inline pointer-events-none" />
-            <span class="text-xs text-accent font-bold">
-                SIGN IN WITH GOOGLE
-            </span>
-        </Button>
-    </a>
-    <div class="font-bold self-end justify-self-end right-2 absolute bottom-2 text-primary">powered by cruX</div>
+
+<div
+	class="background absolute flex h-full w-full select-none flex-col items-center justify-center gap-7 font-bold"
+>
+	<!-- Arena 2025 svg logo because css is hard -->
+	<img src={header} alt="Arena 2025 header" class="pointer-events-none" />
+	<a href="/api/oauth">
+		<Button
+			class="active:bg-grey-300 flex w-64 gap-2 rounded-xl bg-white font-semibold hover:bg-gray-300"
+		>
+			<img src={google} alt="Arena 2025 header" class="pointer-events-none inline" />
+			<span class="text-accent text-xs font-bold"> SIGN IN WITH GOOGLE </span>
+		</Button>
+	</a>
+	<div class="text-primary absolute bottom-2 right-2 self-end justify-self-end text-xl font-bold">
+		powered by cruX
+	</div>
 </div>
+
+<style>
+	.background {
+		background: url('$lib/assets/images/login/spots.png');
+	}
+</style>
