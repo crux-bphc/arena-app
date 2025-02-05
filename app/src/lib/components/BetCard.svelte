dc6bc744a96c7cea308ee4f9c6adadce15d8a5da
skoriop <karthikp.ivy@gmail.com>
2025-02-05 18:52:12 +0530

    feat: add current bets

---
+++ app/src/lib/components/BetCard.svelte
@@ -0,0 +1,127 @@
+<script lang="ts">
+	import type { EventRecWithStandAndBet, StandingsRecordWithTeam } from '$lib/types/expand';
+	import type { BetsResponse } from '$lib/types/pocketbase';
+	import { formatTime, getStatus, getTimeLeft } from '$lib/util/helpers';
+
+	interface BetCardProps {
+		event: EventRecWithStandAndBet;
+		currentBets: BetsResponse[];
+	}
+
+	let { event, currentBets }: BetCardProps = $props();
+	let status: 'finished' | 'ongoing' | 'starting soon' | 'default' = $state(getStatus(event));
+
+	const bets = currentBets.filter((bet) => bet.event === event.id);
+
+	const isMinimized = false;
+
+	const teamNameMap: {[key: string]: string} = {};
+	event.teams.forEach((team) => {
+		teamNameMap[team.id] = team.name;
+	});
+
+	// calculates which color should a team have based on won/lost/ongoing etc
+	function calcColor(standing?: StandingsRecordWithTeam) {
+		if (status == 'ongoing') return 'text-warning';
+		else if (status == 'finished') {
+			return standing?.position === 1 ? 'text-primary' : 'text-foreground/50';
+		}
+		return '';
+	}
+</script>
+
+<div
+	class="bg-secondary flex w-full scroll-mt-24 flex-col items-center justify-between gap-2 {isMinimized
+		? 'rounded-lg p-2'
+		: 'rounded-[10px] p-3'}"
+>
+	<!-- status & title text -->
+	<div
+		class="text-foreground/50 line-clamp-1 w-full text-start {isMinimized
+			? 'text-sm'
+			: 'text-base'}"
+	>
+		<span class="uppercase">
+			{#if status == 'default'}
+				starts at {formatTime(event.startTime)}
+			{:else if status == 'starting soon'}
+				starts in {getTimeLeft(event.startTime)}
+			{:else}{status}{/if}
+		</span>
+		{' - '}
+		<span class="capitalize">
+			{event.title}
+		</span>
+	</div>
+
+	<!-- teams & scoreboard -->
+	<div class="flex w-full flex-col">
+		{#if status == 'default' || status == 'starting soon' || (status == 'ongoing' && !event.standings)}
+			<!-- shows teams if event is yet to start or if ongoing event has no standings-->
+			<div class="w-full text-start font-bold {isMinimized ? 'text-2xl' : 'text-3xl'}">
+				<span class="truncate uppercase">{event.teams.at(0)?.name} {' '}</span>
+				<span class="text-xl">vs {' '}</span>
+				<span class="truncate uppercase">{event.teams.at(1)?.name} {' '}</span>
+
+				<!-- extra teams indicator -->
+				{#if event.teams.length > 2}
+					<span class="text-foreground/50 text-xs font-normal"
+						>(+{event.teams.length - 2} more)</span
+					>
+				{/if}
+			</div>
+		{:else if event.standings}
+			<!-- shows standings if event is ongoing or ended -->
+			<div
+				class="flex w-full flex-row items-center justify-between font-bold {calcColor(
+					event.standings.at(0)
+				)} {isMinimized ? 'text-2xl' : 'text-3xl'}"
+			>
+				<div class="uppercase">{event.standings.at(0)?.team.name}</div>
+				<div class="">{event?.standings.at(0)?.score}</div>
+			</div>
+			<div
+				class="flex w-full flex-row items-center justify-between font-bold {calcColor(
+					event.standings.at(1)
+				)} {isMinimized ? 'text-2xl' : 'text-3xl'}"
+			>
+				<div class="uppercase">{event.standings.at(1)?.team.name}</div>
+				<div class="">{event.standings.at(1)?.score}</div>
+			</div>
+
+			<!-- extra standings indicator -->
+			{#if event.standings.length > 2}
+				<span class="text-foreground/50 text-xs">(+{event.standings.length - 2} more)</span>
+			{/if}
+		{:else}
+			<span class="text-foreground/50 capitalize italic">data unavailable</span>
+		{/if}
+	</div>
+
+	<!-- location text -->
+	{#if status == 'default' || status == 'starting soon'}
+		<div class="flex w-full items-end justify-between {isMinimized ? 'text-sm' : 'text-base'}">
+			<span class="text-foreground/50 mr-1 overflow-hidden truncate text-ellipsis capitalize">
+				{`${event.sport.toString()} - `}{event.location}
+			</span>
+		</div>
+	{:else}
+		<div
+			class="text-foreground/50 mr-1 w-full overflow-hidden truncate text-ellipsis text-start capitalize {isMinimized
+				? 'text-sm'
+				: 'text-base'}"
+		>
+			{`${event.sport.toString()} - `}{event.location}
+		</div>
+	{/if}
+
+	{#each bets as bet}
+		<div
+			class="mr-1 w-full overflow-hidden truncate text-ellipsis text-start font-semibold italic text-[#FFDA46] {isMinimized
+				? 'text-sm'
+				: 'text-base'}"
+		>
+			You've put {bet.amount} points on {teamNameMap[bet.team]}
+		</div>
+	{/each}
+</div>
--- app/src/routes/profile/+page.svelte
+++ app/src/routes/profile/+page.svelte
@@ -1,15 +1,67 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
 	import { PUBLIC_PB_URL } from '$env/static/public';
+	import BetCard from '$lib/components/BetCard.svelte';
+	import EventCard from '$lib/components/EventCard.svelte';
 	import { Avatar, AvatarFallback, AvatarImage } from '$lib/components/ui/avatar';
 	import { Button } from '$lib/components/ui/button';
 	import { Separator } from '$lib/components/ui/separator';
+	import type { EventRecWithStandAndBet } from '$lib/types/expand.js';
+	import type { BetsResponse, EventsResponse } from '$lib/types/pocketbase.js';
 	import { LogOut } from 'lucide-svelte';
+	import { onMount } from 'svelte';
+	import { toast } from 'svelte-sonner';
 
 	let { data } = $props();
+
+	let currentBets: BetsResponse[] = $state([]);
+	let currentBetEvents: EventRecWithStandAndBet[] = $state([]);
+
+	const getBets = async () => {
+		try {
+			let response = await fetch(`api/user/bets?open=true`);
+			let json = await response.json();
+			if (!response.ok) {
+				throw new Error('message' in json ? json.message : `API returned ${response.status}`);
+			}
+			currentBets = json.bets;
+
+			const arr = await Promise.all(
+				currentBets.map(async (bet) => {
+					response = await fetch(`api/event/${bet.event}`);
+					json = await response.json();
+					if (!response.ok) {
+						throw new Error('message' in json ? json.message : `API returned ${response.status}`);
+					}
+					const event = json;
+
+					response = await fetch(`api/event/${bet.event}/standings`);
+					json = await response.json();
+					if (!response.ok) {
+						throw new Error('message' in json ? json.message : `API returned ${response.status}`);
+					}
+
+					event.standings = json.standings;
+					event.teams = event.expand.teams;
+					delete event.expand;
+					return event;
+				})
+			);
+			// remove duplicates by id 
+			currentBetEvents = [...new Map(arr.map((item) => [item['id'], item])).values()];
+			console.log('currentBetEvents', currentBetEvents);
+		} catch (e) {
+			console.error(`Failed to fetch current predictions: ${e}`);
+			toast.error('Failed to fetch current predictions!');
+		}
+	};
+
+	onMount(() => {
+		getBets();
+	});
 </script>
 
-<div class="flex flex-col items-center p-4 gap-6">
+<div class="flex flex-col items-center gap-4 p-4">
 	<div class="flex w-screen flex-row items-center justify-between px-3">
 		<Avatar class="w-21 h-21">
 			<AvatarImage
@@ -31,5 +83,13 @@
 			</div>
 		</div>
 	</div>
-	<Separator />
+	<Separator class="mt-2" />
+	<div class="flex w-screen flex-col gap-4 px-4">
+		<p class="text-lg font-bold">CURRENT PREDICTIONS</p>
+		{#each currentBetEvents as event}
+			{#if event}
+				<BetCard {event} {currentBets}/>
+			{/if}
+		{/each}
+	</div>
 </div>
