17cf9e0ebf99ff7a0cccc59ba68172b3dc2c91c5
Radian6405 <vardhan.6405@gmail.com>
2025-02-06 12:08:25 +0530

    feat: basic page design

--- app/src/routes/leaderboard/+page.svelte
+++ app/src/routes/leaderboard/+page.svelte
@@ -2,7 +2,9 @@
 	import { toast } from 'svelte-sonner';
 	import { onMount } from 'svelte';
 	import Loader from '$lib/components/Loader.svelte';
+	import * as Table from '$lib/components/ui/table';
 	import type { UsersResponse } from '$lib/types/pocketbase';
+	import { Crown } from 'lucide-svelte';
 
 	interface LeaderboardData {
 		users: UsersResponse[];
@@ -14,7 +16,7 @@
 			const response = await fetch(`api/user/leaderboard`);
 			const json = await response.json();
 			if (!response.ok) {
-				throw new Error("message" in json ? json.message : `API returned ${response.status}`);
+				throw new Error('message' in json ? json.message : `API returned ${response.status}`);
 			}
 			leaderboard = json;
 		} catch (e) {
@@ -30,36 +32,27 @@
 
 {#if leaderboard !== null}
 	<div class="mx-auto max-w-2xl overflow-hidden rounded-lg p-4 text-white">
-		<h1 class="mb-6 text-center text-3xl font-bold">Leaderboard</h1>
+		<h1 class="my-6 text-center text-3xl font-bold">Leaderboard</h1>
 
-		<ul class="space-y-6">
-			{#each leaderboard.users as user, index}
-				<li
-					class="grid grid-cols-[0.5fr_2fr_1fr] items-center gap-[16px] rounded-lg bg-gray-800 p-4 shadow-lg"
-				>
-					<span class="position text-center text-xl font-bold">
-						{index + 1}
-					</span>
-					<span class="user-details flex items-center">
-						<span
-							class="ml-4 max-w-[200px] overflow-hidden truncate text-ellipsis text-lg font-semibold"
+		<Table.Root>
+			<Table.Body class="w-screen">
+				{#each leaderboard.users as user, i}
+					<Table.Row class="boorder-b-0 {i == 0 ? 'bg-primary text-primary-foreground' : ''}">
+						<Table.Cell class="w-1/6 text-center text-2xl">
+							{#if i == 0}
+								<Crown class=" text-primary-foreground size-8" />
+							{:else}
+								{i + 1}
+							{/if}
+						</Table.Cell>
+						<Table.Cell class="w-2/3 max-w-[22ch] truncate text-xl font-bold"
+							>{user.name}</Table.Cell
 						>
-							{user.name}
-						</span>
-						{#if index === 0}
-							<span class="ml-2 text-yellow-400">🏆</span>
-						{:else if index === 1}
-							<span class="ml-2 text-gray-300">🥈</span>
-						{:else if index === 2}
-							<span class="ml-2 text-yellow-600">🥉</span>
-						{/if}
-					</span>
-					<span class="text-center text-lg font-bold">
-						{user.balance}
-					</span>
-				</li>
-			{/each}
-		</ul>
+						<Table.Cell class="w-1/6 text-xl">{user.balance}</Table.Cell>
+					</Table.Row>
+				{/each}
+			</Table.Body>
+		</Table.Root>
 	</div>
 {:else}
 	<Loader />
