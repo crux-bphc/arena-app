437e863d010707062ee26314d87f5ffc0a92e0fe
unknown <user.soem1@gmail.com>
2025-02-06 23:40:25 +0530

    fix: commit not showing up on github?

--- app/src/routes/game/+page.svelte
+++ app/src/routes/game/+page.svelte
@@ -1,4 +1,4 @@
 <div class="absolute flex h-full w-full justify-center place-items-center flex-col">
-    <h1 class="m-6 select-none text-center text-3xl font-bold">Game</h1>
+    <h1 class="m-6 select-none text-center text-3xl font-bold">Play Game</h1>
     <iframe src="/game/index.html" title="football-game" class="h-full w-full"></iframe>
 </div>
